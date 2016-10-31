93cdca0551866d81ff6883181bd5e53c36f999e0
Xiaojie Feng <fengxiaojie1997@gmail.com>
2016-10-31 17:52:20 +0800

    add section2 ElementarySorts sample code

---
+++ sample/04ElementarySorts/Insertion.java
@@ -0,0 +1,49 @@
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdRandom;
+
+public class Insertion {
+    public static void sort(Comparable[] a) {
+        int N = a.length;
+        for (int i = 0; i < N; ++i) {
+            for (int j = i; j > 0; --j) {
+                if (less(a[j], a[j-1]))
+                    exch(a, j, j-1);
+                else break;
+            }
+        }
+    }
+
+    private static boolean less(Comparable v, Comparable w) {
+        return v.compareTo(w) < 0;
+    }
+    private static void exch(Comparable[] a, int i, int j) {
+        Comparable swap = a[i];
+        a[i] = a[j];
+        a[j] = swap;
+    }
+
+    private static void show(Comparable[] a) {
+        for (int i = 0; i < a.length; ++i) {
+            StdOut.print(a[i] + " ") ;
+        }
+        StdOut.println();
+    }
+    private static boolean isSorted(Comparable[] a) {
+        for (int i = 1; i < a.length; ++i) {
+            if (less(a[i], a[i-1])) return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int N = 20;
+        Integer[] a = new Integer[N];
+        for (int i = 0; i < N; i++) {
+            a[i] = StdRandom.uniform(0, N);
+        }
+        show(a);
+        sort(a);
+        if (isSorted(a)) show(a);
+    }
+}
