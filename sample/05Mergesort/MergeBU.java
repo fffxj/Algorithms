79843065b938cddb9e32d9b632a8a6e0032a37f0
Xiaojie Feng <fengxiaojie1997@gmail.com>
2016-11-02 13:24:46 +0800

    add section3 sample Mergesort

---
+++ sample/05Mergesort/MergeBU.java
@@ -0,0 +1,66 @@
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdRandom;
+
+public class MergeBU {
+    private static void merge(Comparable[] a, Comparable[] aux, int lo, int mid, int hi) {
+        assert isSorted(a, lo, mid);
+        assert isSorted(a, mid+1, hi);
+
+        for (int k = lo; k <= hi; ++k)
+            aux[k] = a[k];
+
+        int i = lo, j = mid+1;
+        for (int k = lo; k <= hi; ++k) {
+            if      (i > mid)              a[k] = aux[j++];
+            else if (j > hi)               a[k] = aux[i++];
+            else if (less(aux[i], aux[j])) a[k] = aux[i++];
+            else                           a[k] = aux[j++];
+        }
+
+        assert isSorted(a, lo, hi);
+    }
+
+    public static void sort(Comparable[] a) {
+        int n = a.length;
+        Comparable[] aux = new Comparable[n];
+        for (int len = 1; len < n; len *= 2) {
+            for (int lo = 0; lo < n-len; lo += len+len) {
+                int mid = lo+len-1;
+                int hi = Math.min(lo+len+len-1, n-1);
+                merge(a, aux, lo, mid, hi);
+            }
+        }
+        assert isSorted(a);
+    }
+
+    private static boolean less(Comparable v, Comparable w) {
+        return v.compareTo(w) < 0;
+    }
+    private static void show(Comparable[] a) {
+        for (int i = 0; i < a.length; ++i) {
+            StdOut.print(a[i] + " ") ;
+        }
+        StdOut.println();
+    }
+    private static boolean isSorted(Comparable[] a) {
+        return isSorted(a, 0, a.length - 1);
+    }
+    private static boolean isSorted(Comparable[] a, int lo, int hi) {
+        for (int i = lo+1; i <= hi; ++i) {
+            if (less(a[i], a[i-1])) return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int N = 20;
+        Integer[] a = new Integer[N];
+        for (int i = 0; i < N; i++) {
+            a[i] = StdRandom.uniform(0, N);
+        }
+        show(a);
+        sort(a);
+        if (isSorted(a)) show(a);
+    }
+}
---
+++ sample/05Mergesort/MergeX.java
@@ -0,0 +1,82 @@
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdRandom;
+
+public class MergeX {
+    private static final int CUTOFF = 7;
+
+    private static void merge(Comparable[] src, Comparable[] dst, int lo, int mid, int hi) {
+        assert isSorted(src, lo, mid);
+        assert isSorted(src, mid+1, hi);
+
+        int i = lo, j = mid+1;
+        for (int k = lo; k <= hi; ++k) {
+            if      (i > mid)              dst[k] = src[j++];
+            else if (j > hi)               dst[k] = src[i++];
+            else if (less(src[i], src[j])) dst[k] = src[i++];
+            else                           dst[k] = src[j++];
+        }
+
+        assert isSorted(dst, lo, hi);
+    }
+    public static void sort(Comparable[] src, Comparable[] dst, int lo, int hi) {
+        // if (hi <= lo) return;
+        if (hi <= lo + CUTOFF) {
+            insertionSort(dst, lo, hi);
+            return;
+        }
+        int mid = lo + (hi - lo) / 2;
+        sort(dst, src, lo, mid);
+        sort(dst, src, mid+1, hi);
+
+        if (less(src[mid], src[mid+1])) return;
+        merge(src, dst, lo, mid, hi);
+    }
+    public static void sort(Comparable[] a) {
+        Comparable[] aux = a.clone();
+        sort(aux, a, 0, a.length-1);
+        assert isSorted(a);
+    }
+
+    private static void insertionSort(Comparable[] a, int lo, int hi) {
+        for (int i = lo; i <= hi; ++i)
+            for (int j = i; j > lo && less(a[j], a[j-1]); --j)
+                exch(a, j, j-1);
+    }
+
+    private static boolean less(Comparable v, Comparable w) {
+        return v.compareTo(w) < 0;
+    }
+    private static void exch(Comparable[] a, int i, int j) {
+        Comparable swap = a[i];
+        a[i] = a[j];
+        a[j] = swap;
+    }
+
+    private static void show(Comparable[] a) {
+        for (int i = 0; i < a.length; ++i) {
+            StdOut.print(a[i] + " ") ;
+        }
+        StdOut.println();
+    }
+    private static boolean isSorted(Comparable[] a) {
+        return isSorted(a, 0, a.length - 1);
+    }
+    private static boolean isSorted(Comparable[] a, int lo, int hi) {
+        for (int i = lo+1; i <= hi; ++i) {
+            if (less(a[i], a[i-1])) return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int N = 20;
+        Integer[] a = new Integer[N];
+        for (int i = 0; i < N; i++) {
+            a[i] = StdRandom.uniform(0, N);
+        }
+        show(a);
+        sort(a);
+        if (isSorted(a)) show(a);
+    }
+}
